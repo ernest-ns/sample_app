850c8d15e5c32376952759a05eb2026efbba6da6
Ernest <ernest.sankeshwar@gmail.com>
2010-12-17 19:24:11 +0530

    Done with static pages

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,14 @@ module SampleApp
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
+    
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
